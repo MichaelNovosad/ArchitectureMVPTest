f0884b6e295c066278c7efc86c32e46ecbae7952
Michael Novosad <84763034+MichaelNovosad@users.noreply.github.com>
2023-07-17 20:42:21 +0300

    Add files via upload

---
+++ ArchitectureTestMVP/ArchitectureTestMVP/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  ArchitectureTestMVP
+//
+//  Created by Michael Novosad on 18.09.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
